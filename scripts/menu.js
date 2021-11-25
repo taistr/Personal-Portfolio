e89cd4b7981e7f1689e6c6fdf44b038a3926280f
Tyson Truong <tystruong@gmail.com>
2021-11-26 10:14:52 +1100

    Added interactive emojis to intro section and fixed emoji rendering

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,22 @@
 "use strict"
 
+let emojis = ["1F44B", "1F44F", "1F44A", "1F44C"];
+let emojiCount = 0;
+
 function toggleMenu() {
     let menu = document.querySelector(".menu");
     menu.classList.toggle("active");
 }
 
+function cycleEmoji() {
+    let emojiRef = document.getElementById("fun_emoji");
+    
+    if (emojiCount == emojis.length - 1) {
+        emojiCount = 0;
+    } else {
+        emojiCount++;
+    }
 
+    emojiRef.innerHTML = String.fromCodePoint("0x" + emojis[emojiCount]);
+
+}
